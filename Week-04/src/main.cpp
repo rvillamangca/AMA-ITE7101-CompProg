c5fafd2f1eeb7f7258ce908697585cd3ef79ae79
rvillamangca <141865388+rvillamangca@users.noreply.github.com>
2023-08-12 14:03:06 +0300

    Update2-08112023

---
+++ Week-04/src/main.cpp
@@ -0,0 +1,66 @@
+/* ****************************************************************
+ * @author: Ramon Villamangca
+ * @app name: Laboratory 003
+ * @app desc: Completion of a laboratory exercise for ITE-17101 (Computer Programming)
+ * @history:
+ *  - 2023/08/11 | Solutions for Lab Exercise
+ *      -- Laboratory Exercise 003
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include "_pause.h"
+
+using namespace std;
+
+//////////////////////////////////////////////////////////////////
+//                               NOTE
+// This is your program entry point. Your main logic is placed
+// inside this function. You may add your functions before this
+// "main()", or after this "main()" provided you added reference
+// before this "main()" function.
+//////////////////////////////////////////////////////////////////
+
+int main() {
+
+
+    // ************************** TO DO **************************
+    // Place your code logic after this comment line
+    // ***********************************************************
+
+    //Solution 01:
+    printf("\nSolution 01 - Two(2) Lines:\n\n");
+    cout << "Hello" << endl;            //1
+    cout << "World" << endl << endl;    //2
+
+    //Solution 02:
+    /*printf("\nSolution 02 - Ten(10) Lines:\n\n");
+    cout << 'H';                        //1
+    cout << 'e';                        //2
+    cout << 'l';                        //3
+    cout << 'l';                        //4
+    cout << 'o' << endl;                //5
+    cout << 'W';                        //6
+    cout << 'o';                        //7
+    cout << 'r';                        //8
+    cout << 'l';                        //9
+    cout << 'd' << endl << endl;        //10*/
+
+    //Solution 03:
+    /*printf("\nSolution 03 - Six(6) Lines:\n\n");
+    cout << "Hello" << endl;            //1
+    cout << 'W';                        //2
+    cout << 'o';                        //3
+    cout << 'r';                        //4
+    cout << 'l';                        //5
+    cout << 'd' << endl << endl;        //6*/
+
+
+
+    // ********************** DO NOT CHANGE **********************
+    // Print a new line and ask user for any key before exiting
+    // ***********************************************************
+    _pause();
+    return EXIT_SUCCESS;
+}
